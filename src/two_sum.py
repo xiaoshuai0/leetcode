1ab2deae74b57c1400fdfa6f9a105c4462c66c65
hzf-zhaoshuai <zhaoshuai@huizhaofang.com>
2021-02-23 23:31:25 +0800

    two_some

---
+++ src/two_sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums, target):
+        map = {}
+        for (index, value) in enumerate(nums):
+            if target - value in map:
+                return [map[target - value], index]
+            map[value] = index
+        return []
+    
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.twoSum([2, 7, 11, 15], target=9))
